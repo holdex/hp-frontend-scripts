be6f6dfcce73cc5d3a273d4a3e014bf0defec997
georgeciubotaru <george@holdex.io>
2019-05-29 17:03:49 +0300

    feature: add support for typescript

--- config/paths.js
+++ config/paths.js
@@ -58,7 +58,7 @@ module.exports = {
 	appBuild: resolveApp('build'),
 	appPublic: resolveApp('public'),
 	appHtml: resolveApp('public/index.html'),
-	appIndexJs: resolveApp('src/index.js'),
+	appIndexJs: resolveApp('src/index'),
 	appTsConfig: resolveApp('tsconfig.json'),
 	appPackageJson: resolveApp('package.json'),
 	appSrc: resolveApp('src'),
